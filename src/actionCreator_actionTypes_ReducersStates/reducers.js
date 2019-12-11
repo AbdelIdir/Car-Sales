898815f53d2a2a893bf948e00fd289e941c67f24
AbdelIdir <homeworkoracle@protonmail.com>
2019-12-11 16:52:44 +0100

    finished making reducer functions and logic for the app

---
+++ src/actionCreator_actionTypes_ReducersStates/reducers.js
@@ -0,0 +1,59 @@
+import * as types from "./actionTypes";
+
+const initialAdditionalPrice = 0;
+export function priceReducer(price = initialAdditionalPrice, action) {
+  switch (action.type) {
+    case types.CALC_TOTAL:
+      return price + action.payload.addedNewPrice;
+    default:
+      return price;
+  }
+}
+
+const initialCar = {
+  price: 26395,
+  name: "2019 Ford Mustang",
+  image:
+    "https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg",
+  features: []
+};
+
+export function carReducer(car = initialCar, action) {
+  switch (action.type) {
+    case types.ADD_FEATURE:
+      return {
+        ...car,
+        features: [
+          ...car.features.find(randomFeature => {
+            return randomFeature.id === action.payload.id;
+          })
+        ]
+      };
+    case types.REMOVE_FEATURE:
+      return {
+        ...car,
+        features: [
+          ...car.features.filter(randomFeature => {
+            return randomFeature.id === action.payload.id;
+          })
+        ]
+      };
+  }
+}
+
+const state = {
+  additionalPrice: 0,
+  car: {
+    price: 26395,
+    name: "2019 Ford Mustang",
+    image:
+      "https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg",
+    features: []
+  },
+  additionalFeatures: [
+    { id: 1, name: "V-6 engine", price: 1500 },
+    { id: 2, name: "Racing detail package", price: 1500 },
+    { id: 3, name: "Premium sound system", price: 500 },
+    { id: 4, name: "Rear spoiler", price: 250 }
+  ]
+};
